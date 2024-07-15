ebcd3e96960f2da165f0f77f390b4ba512846e3e
root <root@LAPTOP-0FFUURN6.localdomain>
2024-07-15 13:34:30 +0200

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,14 @@
-#  fix our stack so that we get to 0
-file { 'replace last line':
-    ensure  => present,
-    path    => '/etc/default/nginx',
-    content => 'ULIMIT="-n 4096"',
+# Increases the amount of traffic an Nginx server can handle.
+
+# Increase the ULIMIT of the default file
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-service { 'nginx':
-    ensure    => running,
-    subscribe => File['/etc/default/nginx']
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
