8c1d484937b107ebb4402c19faf4a5857fb472bf
root <root@LAPTOP-0FFUURN6.localdomain>
2024-07-15 12:40:32 +0200

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,11 @@
-# This manuscript increases the amount of traffic an Nginx server can handle
-
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+#  fix our stack so that we get to 0
+file { 'replace last line':
+    ensure  => present,
+    path    => '/etc/default/nginx',
+    content => 'ULIMIT="-n 4096"',
 }
 
-# Restart Nginx
--> exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/default/nginx']
 }
